05b699b5b265f818a48058bee400561c0441889a
Gianluca Consoli <gianluca.consoli@newteam.it>
2026-05-11 17:03:25 +0200

    Added some Binding Syntax and Directives to exercise

---
+++ hotelinventoryapp/src/app/rooms/irooms.ts
@@ -0,0 +1,5 @@
+export interface IRooms {
+    totalRooms?: number;
+    avaibleRooms?: number;
+    bookRooms?: number;
+}
--- hotelinventoryapp/src/app/rooms/rooms.ts
+++ hotelinventoryapp/src/app/rooms/rooms.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { IRooms } from './irooms';
 
 @Component({
   selector: 'hinv-rooms',
-  imports: [],
   templateUrl: './rooms.html',
   styleUrl: './rooms.scss',
 })
 export class Rooms {
+  hotelName = 'Hotel Test App';
+  numberOfRooms = 50;
+  hideRooms = false;
 
+  rooms : IRooms = {
+    totalRooms: 20,
+    avaibleRooms: 10,
+    bookRooms: 5  
+  };
+
+    //totalRooms: 20,
+    //avaibleRooms: 10,
+    //bookRooms: 5
+
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+  }
 }
